6750671f33e0da3ec00471b6c619f18371bfd95e
asease42 <asease42@gmail.com>
2018-07-04 00:27:52 +0300

    Simple Portfolio Template

---
+++ js/style.js
@@ -0,0 +1,24 @@
+
+$(".twitter").hover(function () {
+	$(".change-bg").toggleClass("color-twitter");
+	$("#h1c").toggleClass("h1w");
+});
+
+$(".facebook").hover(function () {
+	$(".change-bg").toggleClass("color-facebook");
+	$("#h1c").toggleClass("h1w");
+});
+$(".github").hover(function () {
+	$(".change-bg").toggleClass("color-github");
+	$("#h1c").toggleClass("h1w");
+});
+
+$(".instagram").hover(function () {
+	$(".change-bg").toggleClass("color-instagram");
+	$("#h1c").toggleClass("h1w");
+});
+$(".linkedin").hover(function () {
+	$(".change-bg").toggleClass("color-linkedin");
+	$("#h1c").toggleClass("h1w");
+});
+//circle javascript codes here
